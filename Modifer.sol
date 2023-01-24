26d907784fdd986d619bb76cf60b8208420efe7d
Jignesh120404 <120722412+Jignesh120404@users.noreply.github.com>
2023-01-24 19:50:14 +0530

    Add files via upload

---
+++ Modifer.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.17 and less than 0.9.0
+pragma solidity ^0.8.17;
+
+contract HelloWorld {
+    address admin;
+    modifier onlyadmin(){
+        require(msg.sender== admin)
+        _;
+    }
+    function MyContract() public onlyadmin{
+        
+    }
+}
